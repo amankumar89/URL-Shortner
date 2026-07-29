4fd6ebc768674993ec06951719ddae8c69bfbd5b
amankumar89 <amankumaroo784@gmail.com>
2026-07-29 10:33:10 +0530

    added api base url

--- frontend/src/lib/http.ts
+++ frontend/src/lib/http.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000/api";
+export let API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000";
+API_BASE_URL += "/api";
 
 export const http = axios.create({
   baseURL: API_BASE_URL,
